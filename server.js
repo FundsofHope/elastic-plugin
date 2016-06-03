9838da4cd59a2ccd487ddad75bb8f6cb6a5664b1
Aditya <addiittya2006@gmail.com>
2016-06-03 10:34:13 +0530

    init search

--- server.js
+++ server.js
@@ -1,16 +1,56 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+var elasticsearch = require('elasticsearch');
 var app = express();
 
 app.use(bodyParser());
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('index',{})
+});
+
+var elastic = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+app.post('/saveUser', function (req, res) {
+    var user = new User({
+        name : req.body.name,
+        email : req.body.email,
+        username : req.body.username
+    });
+    user.save(function (err) {
+        if (err) throw err;
+        console.log('User saved successfully');
+        res.json({
+            success: true
+        });
+    });
+});
+
+app.post('/search', function (req, res) {
+     elastic.search({
+      index: 'users',
+      type: 'user',
+      body: {
+        query: {
+          fuzzy: {
+             _all: req.body.searchterm
+          }
+        }
+      }
+  }).then(function(response) {
+      var success = response.hits.hits;
+      console.log("Got a Hit!");
+      res.send(success);
+  }, function(error) {
+      console.trace(error.message);
+      res.send(404);
+  });
 });
 
 app.listen(3000);
